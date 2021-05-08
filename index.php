dbd478a637c9f0ebcbcb00a0ab0f0d2b5374412f
Shahzeb Malik <33540322+ShahzebMalik107@users.noreply.github.com>
2021-05-08 22:04:22 +0500

    Progress
    About section

--- index.php
+++ index.php
@@ -8,10 +8,21 @@
 </head>
 <body>
     <?php require_once("header.php")?>
-    <section class="banner">
-        /<div class="banner__main container">
+    <section id="#home" class="banner">
+        <div class="banner__main container">
         <h1>Shahzeb Malik</h1>
         <h2>Web Problem Solver</h2>
+        </div>
+    </section>
+    <section class="About" id="#About">
+        <div class="About__main container">
+            <div class="About__main__profile">
+                &ensp;
+            </div>
+            <div class="About__main__text">
+                <h1>About Me</h1>
+                <p>I'm a person not afraid of any kind of challenge. Someone who can implement design through various programming languages and CMS. All these descriptions can go for a good front-end developer. But who is this software professional whose work we can see everywhere on the Internet and without whom we wouldn't be able to enjoy our favorite websites the way we do? It's someone with extensive knowledge of technologies like HTML, CSS, and JavaScript. Someone who gives you (a user) the opportunity to interact with the website and website applications directly. And finally, someone that makes sure you can open up and browse the site on any device or in any browser and the information will still be relevant and easy to read. For me, communication plays an instrumental role in the development process and I strive to keep my clients involved every stage. My penchant for detail allows me to balance form and function every step of the way.</p>  
+            </div>
         </div>
     </section>
 </body>
