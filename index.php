45edad4108d6d67ad710cd15ad83dcab22248a32
Shahzeb Malik <33540322+ShahzebMalik107@users.noreply.github.com>
2021-05-10 03:54:56 +0500

    progress

--- index.php
+++ index.php
@@ -1,29 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shahzeb Malik</title>
 </head>
+
 <body>
-    <?php require_once("header.php")?>
+    <?php require_once("header.php") ?>
     <section id="#home" class="banner">
         <div class="banner__main container">
-        <h1>Shahzeb Malik</h1>
-        <h2>Web Problem Solver</h2>
+            <h1>Shahzeb Malik</h1>
+            <h2>Web Problem Solver</h2>
         </div>
     </section>
-    <section class="About" id="#About">
+    <section class="About" id="About">
         <div class="About__main container">
             <div class="About__main__profile">
                 &ensp;
             </div>
             <div class="About__main__text">
                 <h1>About Me</h1>
-                <p>I'm a person not afraid of any kind of challenge. Someone who can implement design through various programming languages and CMS. All these descriptions can go for a good front-end developer. But who is this software professional whose work we can see everywhere on the Internet and without whom we wouldn't be able to enjoy our favorite websites the way we do? It's someone with extensive knowledge of technologies like HTML, CSS, and JavaScript. Someone who gives you (a user) the opportunity to interact with the website and website applications directly. And finally, someone that makes sure you can open up and browse the site on any device or in any browser and the information will still be relevant and easy to read. For me, communication plays an instrumental role in the development process and I strive to keep my clients involved every stage. My penchant for detail allows me to balance form and function every step of the way.</p>  
+                <p>I'm a person not afraid of any kind of challenge. Someone who can implement design through various programming languages and CMS. All these descriptions can go for a good front-end developer. But who is this software professional whose work we can see everywhere on the Internet and without whom we wouldn't be able to enjoy our favorite websites the way we do? It's someone with extensive knowledge of technologies like HTML, CSS, and JavaScript. Someone who gives you (a user) the opportunity to interact with the website and website applications directly. And finally, someone that makes sure you can open up and browse the site on any device or in any browser and the information will still be relevant and easy to read. For me, communication plays an instrumental role in the development process and I strive to keep my clients involved every stage. My penchant for detail allows me to balance form and function every step of the way.</p>
             </div>
         </div>
     </section>
+    <section class="skills linebeak" id="Skills">
+        <div class="skills__main container">
+
+            <div>
+                <h3>HTML/CSS</h3>
+            </div>
+            <div>
+                <h3>WordPress</h3>
+            </div>
+            <div>
+                <h3>JavaScript</h3>
+            </div>
+        </div>
+    </section>
+    <section class="skill__details">
+        <div class="container">
+            <ul class=" skill__detals_main">
+                <li class="html skill__details--container">
+                    <span>
+                        <h4>HTML/CSS</h4>
+                        <span></span>
+                    </span>
+                </li>
+                <li class="BootStrap skill__details--container">
+                    <span>
+                        <h4>BootStrap</h4>
+                        <span></span>
+                    </span>
+                </li>
+                <li class="JavaScript skill__details--container">
+                    <span>
+                        <h4>JavaScript</h4>
+                        <span></span>
+                    </span>
+                </li>
+                <li class="html skill__details--container">
+                    <span>
+                        <h4>Wordpress</h4>
+                        <span></span>
+                    </span>
+                </li>
+                <li class="React skill__details--container">
+                    <span>
+                        <h4>React</h4>
+                        <span></span>
+                    </span>
+                </li>
+                <li class="PHP skill__details--container">
+                    <span>
+                        <h4>PHP</h4>
+                        <span></span>
+                    </span>
+                </li>
+                <li class="photoshop skill__details--container">
+                    <span>
+                        <h4>Photoshop</h4>
+                        <span></span>
+                    </span>
+                </li>
+                <li class="XD skill__details--container">
+                    <span>
+                        <h4>XD</h4>
+                        <span></span>
+                    </span>
+                </li>
+            </ul>
+        </div>
+    </section>
 </body>
+
 </html>
